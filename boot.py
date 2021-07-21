34ec6642fc05f668b4206ccfaa355d3b08ceecae
Billfred <44166106+Billfred@users.noreply.github.com>
2021-07-21 13:29:08 -0400

    Uploaded boot.py and code.py
    boot.py for examination (because it's kicking my butt) and code.py (which is working fine on a PC)

---
+++ boot.py
@@ -0,0 +1,22 @@
+import board
+import digitalio
+import storage
+import usb_cdc
+import usb_hid
+
+pin5 = digitalio.DigitalInOut(board.A5)
+pin4 = digitalio.DigitalInOut(board.A4)
+pin4.switch_to_input(pull=digitalio.Pull.UP)
+pin5.switch_to_input(pull=digitalio.Pull.UP)
+notpin4 = not pin4
+notpin5 = not pin5
+
+if notpin4 and notpin5:
+    storage.disable_usb_drive()  # disable CIRCUITPY USB drive
+    usb_cdc.disable()            # disable REPL
+    usb_midi.disable()           # disable MIDI
+
+usb_hid.enable(devices=(usb_hid.Device.KEYBOARD))
+
+pin4.deinit()
+pin5.deinit()
